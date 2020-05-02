be928d7cc9c0e3cc924492406dc9a9bfdf81f789
Michael Cho <55390802+mke680@users.noreply.github.com>
2020-05-02 15:40:24 +1000

    Added Resource Group Creation .tf

---
+++ Terraform/CreateRG.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+  subscription_id = "faaae9cc-6d6c-4370-adab-afc917594c0d"
+}
+resource "azurerm_resource_group" "rg" {
+        name = "testResourceGroup0201"
+        location = "australiaeast"
+}
