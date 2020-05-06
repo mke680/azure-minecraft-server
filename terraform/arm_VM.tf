befe8dde35dfeea5723b1334c3d9430aabd38c1f
Michael Cho <mcfallenangel@gmail.com>
2020-05-06 10:35:25 +1000

    Adding-VM Subnet as String

--- terraform/arm_VM.tf
+++ terraform/arm_VM.tf
@@ -2,7 +2,7 @@ resource "azurerm_subnet" "sub" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = "172.16.2.0/24"
+  address_prefixes     = ["172.16.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
